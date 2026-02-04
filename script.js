4a7dff0ce169681d50326647619ff4eeca2fcde7
lindsayowow <lindsayow@gmail.com>
2026-02-04 14:30:20 -0600

    cleaning up code for GET data before adding POST

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 const divList = document.getElementById("postList"); // I brought most all elements in because I'm not sure which I will need
-const divForm = document.getElementById("postForm");
 const divError = document.getElementById("formError"); //I can't seem to get this div to work in the catch phrase. left it in in case I can fix it later
 const divSuccess = document.getElementById("formSuccess");
 const divErrorMessage = document.getElementById("errorMessage");
 const fetchGetButton = document.getElementById("fetchButton");
-const submitSendButton = document.getElementById("submitButton");
 const sendTitle = document.getElementById("titleInput");
 const sendBody = document.getElementById("bodyInput");
+const divPost = document.getElementById("postForm");
 
-divList.innerHTML = `Loading...`
+//Event listener to post the data to the server.
+divPost.addEventListener("submit", function () {})
 
-// Wait for the page to load
-window.addEventListener("load", function () {
+
     // Make the GET request from the site to get a title/body
     fetchGetButton.addEventListener("click", function () {
-        let selectBookNumber = Math.floor(Math.random() * 100) + 1; // this gets a random index number from the object list
-
-        fetch("https://jsonplaceholder.typicode.com/posts/break")
+        let selectBookNumber = Math.floor(Math.random() * 100); // this gets a random index number from the object list
+        divList.innerHTML = `Loading...`
+        fetch("https://jsonplaceholder.typicode.com/posts")
             .then(function (response) {
                 // Convert the response to JSON
                 return response.json();
@@ -25,7 +24,7 @@ window.addEventListener("load", function () {
                 console.log(json); // Log the JSON data to console, next line adds it to the html
                 divList.innerHTML = `  
     <ul>
-        <li>Title: ${json[selectBookNumber].title}</li><br>
+        <li>Title: ${json[selectBookNumber].title}</li>
         <li>Body: ${json[selectBookNumber].body}</li>
     </ul>
 `;
@@ -35,4 +34,4 @@ window.addEventListener("load", function () {
                 divList.innerHTML = `Error fetching the data.`
             });
     });
-});
+
